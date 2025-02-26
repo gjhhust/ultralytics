cacd5c8104a9137e536dbee1a7d81d1625358623
郭嘉豪 <gjh_hust@foxmail.com>
2025-02-26 19:39:42 +0800

    “test”

--- tools/train_baseline.py
+++ tools/train_baseline.py
@@ -2,11 +2,11 @@ from ultralytics import YOLO
 
 
 # Load a COCO-pretrained RT-DETR-l model
-# model = YOLOFT("yoloft/train113/weights/last.pt") #resume
+model = YOLO("/data/jiahaoguo/ultralytics_yoloft/ultralytics/runs/detect/train70/weights/last.pt") #resume
 # model = YOLOFT("config/yoloft_dev/yoloftv2-C_DCN-L.yaml").load("yolov8l.pt") #train yoloft-l
-model = YOLO("config/yolo/yolov8_L.yaml").load("yolov8l.pt") #train yolov8-l
+# model = YOLO("config/yolo/yolov8_S.yaml").load("yolov8s.pt") #train yolov8-l
 # model = YOLO("/data/shuzhengwang/project/ultralytics/runs/detect/train15/weights/epoch2.pt")
 
 results = model.train(data="config/dataset/XS-VIDv2.yaml",
                       cfg="config/train/default.yaml", 
-                      batch=6*2, epochs=7, imgsz=1920, device=[2,3],workers = 6)
+                      batch=12*2, epochs=20, imgsz=1024, device=[2,3],workers = 6)
