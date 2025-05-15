40d3a11da8d7ed31edd2967cfc56fe473ce3ef29
Mateo-Wang <w1245561979@qq.com>
2025-05-15 10:10:55 +0800

    ss

--- tools/train_yoloft.py
+++ tools/train_yoloft.py
@@ -9,9 +9,9 @@ from ultralytics import YOLOFT, YOLO
 # model = YOLO("config/yoloft/yoloftsv2-C-384.yaml").load("./yoloft-C-384_obj365_e7.pt")
 model = YOLO("config/yolo/yolov8_S1_DCN_dy.yaml").load("pretrain/yolov8_S1_DCN_dy/best.pt")
 
-results = model.train(data="config/dataset/XS-VIDv2_stream.yaml",
+results = model.train(data="config/dataset/XS-VIDv2.yaml",
                       cfg="config/train/default.yaml",
                       # constrained = True,
-                      batch=30*2, epochs=25, imgsz=1024, device=[2,3],workers = 4)
+                      batch=30, epochs=25, imgsz=1024, device=[0],workers = 4)
                     # batch=6, epochs=25, imgsz=896, device=[2],workers = 6)
 # print("yoloftsv2-C-384_cbam_Focus \nload(./yolov8s.pt)  \nTrain_6_Test_gaode6")
